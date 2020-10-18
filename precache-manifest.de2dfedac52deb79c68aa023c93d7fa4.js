e227c6a6da2304c9191374429666b4dbeb016935
Vengat750 <venkivkl008@gmail.com>
2020-10-18 11:27:53 +0530

    Updates

---
+++ precache-manifest.de2dfedac52deb79c68aa023c93d7fa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd10d7b8797bebcaf741d71f2ba0a6a",
+    "url": "/registration-form/index.html"
+  },
+  {
+    "revision": "880e3ebf7fed44bf348a",
+    "url": "/registration-form/static/css/main.36ac6385.chunk.css"
+  },
+  {
+    "revision": "5eced423495ea55f8c76",
+    "url": "/registration-form/static/js/2.8033d54f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/registration-form/static/js/2.8033d54f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880e3ebf7fed44bf348a",
+    "url": "/registration-form/static/js/main.04c150ce.chunk.js"
+  },
+  {
+    "revision": "ef3fa5b86aab39cb7f4d",
+    "url": "/registration-form/static/js/runtime-main.84d6f335.js"
+  },
+  {
+    "revision": "64c8edd64e99e2194efbd7c814d1609f",
+    "url": "/registration-form/static/media/Logo.64c8edd6.png"
+  }
+]);
